c3d40d75bb52cf28d40eabf1a46fc50891111f55
AlejandraG12 <163395423+AlejandraG12@users.noreply.github.com>
2024-06-05 11:50:14 +0200

    jun5

---
+++ js/ejercicios/piedra-papel-tijera.js
@@ -0,0 +1,25 @@
+let jugada;
+let juagadaCPU;
+let puntuacion;
+
+const opciones=[
+
+    'piedra',
+    'papel',
+    'tijera'
+]
+
+function elegir(jugada) {
+    
+    console.log('Has elegido '+ jugada);
+    
+}
+
+function elegirCpu() {
+
+    // elegir número random 0-2
+    // Opciones [numeros random]
+    // console.log( cpu ha elegido)
+
+    
+}
